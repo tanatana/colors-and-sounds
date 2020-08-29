bac89a27ba3f0d582a1635338d5cda10e5012c5f
Yusuke Tsuji <tugusuke@gmail.com>
2020-08-29 15:28:48 +0900

    improve touch handling

--- pages/pad.js
+++ pages/pad.js
@@ -1,4 +1,4 @@
-import HEAD from 'next/head'
+import Head from 'next/head'
 import { useState, useEffect } from 'react';
 import styles from '../styles/Controller.module.css'
 
@@ -16,7 +16,7 @@ export default function Pad({x, y}) {
     console.log('init')
     // create web audio api context
     const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
+    console.log(audioCtx)
     // create Oscillator node
     var oscillator = audioCtx.createOscillator();
 
@@ -32,6 +32,15 @@ export default function Pad({x, y}) {
         setGainNode(gainNode)
         setOscillator(oscillator)
     })()
+
+    // iOS 11
+    document.documentElement.addEventListener('touchstart', (e) => {
+      console.log('document touchstat stopPropagation')
+      if (e.touches && e.touches.length > 1) {
+        e.preventDefault()
+        e.stopPropagation()
+      }
+    }, {passive: false})
   }, []);
 
   function pointerMoveEventHandlerWithThrottole(e, msec) {
@@ -52,22 +61,40 @@ export default function Pad({x, y}) {
         clientX = e.targetTouches[0].clientX
         clientY = e.targetTouches[0].clientY
     }
+    playOscillator(clientX, clientY, e.target.clientWidth, e.target.clientHeight)
+  }
 
-    const color = {h: clientX/e.target.clientWidth*100*(Math.PI/2.5), s: (50 - (clientY/e.target.clientHeight)*50)+50, l:70}
+  function playOscillator(clientX, clientY, clientWidth, clientHeight) {
+    const color = {h: clientX/clientWidth*150+15, s: (50 - (clientY/clientHeight)*50)+50, l:55}
     setColor(color)
     if (gainNode !== null) {
-        gainNode.gain.value = (1 - (clientY/e.target.clientHeight))*0.15
+        gainNode.gain.value = (1 - (clientY/clientHeight))*0.15
     }
     if (oscillator !== null) {
-        oscillator.frequency.value = 240 + (clientX/e.target.clientWidth)*400; // value in hertz
+        oscillator.frequency.value = 240 + (clientX/clientWidth)*400; // value in hertz
     }
   }
 
   function pointerDownEventHandler(e) {
+    console.log('down')
+    e.preventDefault()
+    e.stopPropagation()
+
     setPointerIsDown(true)
-    if (oscillator !== null && !oscillatorStarted) {
-        oscillator.start()
-        setOscillatorStarted(true)
+    if (oscillator !== null) {
+        if (!oscillatorStarted) {
+            oscillator.start()
+            setOscillatorStarted(true)
+        }
+
+        let clientX = e.clientX
+        let clientY = e.clientY
+            if (clientX === undefined && e.targetTouches[0] !== undefined) {
+            clientX = e.targetTouches[0].clientX
+            clientY = e.targetTouches[0].clientY
+        }
+        console.log('play')
+        playOscillator(clientX, clientY, e.target.clientWidth, e.target.clientHeight)
     }
   }
 
@@ -87,6 +114,19 @@ export default function Pad({x, y}) {
       onMouseUp={e => pointerUpEventHndler(e)}
       onTouchEnd={e => pointerUpEventHndler(e)}
       onMouseMove={e => pointerMoveEventHandlerWithThrottole(e, 16)}
-      onTouchMove={e => pointerMoveEventHandlerWithThrottole(e, 32)}> </div>
-  )
+      onTouchMove={e => pointerMoveEventHandlerWithThrottole(e, 32)}>
+        <Head>
+          <title>Swipe to play</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"></meta>
+          <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+          <meta name="apple-mobile-web-app-status-bar-style" content="black"></meta>
+        </Head>
+        <style jsx global>{`
+          body {
+            background-color: hsl(${color.h}, ${color.s}%, ${color.l}%);
+          }
+        `}</style>
+      </div>
+   )
 }
